859163314a5c7b88a884f35a60aacd62d689aef2
JnthnTrn <jonathantran0106@gmail.com>
2020-10-19 18:29:56 -0700

    Loops + Lists_Independent_Challenge

---
+++ Challenges/Lists_Independent_Challenge.py
@@ -0,0 +1,31 @@
+# Challenge 1
+# def append_sum(lst):
+#     list.append([-1] + [-2])
+#     list.append([-1] + [-2])
+#     list.append([-1] + [-2])
+#     return lst
+# print(append_sum([1, 1, 2]))
+
+# Challenge 2 
+# Key takeaways
+# 1. Learned how to traverse a list backwards [-1] that always gets us the last element of any size list
+# 2. elifs (can use as many as we need/want)
+# def larger_list(lst1, lst2):
+#     if len(lst1) == len(lst2):
+#         return (lst1)[-1]
+#     elif len(lst1) < len(lst2):
+#         return lst2[-1]
+#     elif len(lst1) > len(lst2):
+#         return (lst1)[-1]
+
+# print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+# Challenge 3
+# def more_than_n(lst, item, n):
+#     if lst.count(item) > n:
+#         return True
+#     else:
+#         return False
+# print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
+
+# Challenge 4
---
+++ loops.py
@@ -0,0 +1,39 @@
+# names = ["alex", "allistair", "andrew", "angelo", "elijah", "george", "jonathan"]
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
+# print(names[4])
+# print(names[5])
+# print(names[6])
+
+# names = ["alex", "allistair", "andrew", "angelo", "elijah", "george", "jonathan"]
+# for name in names:
+#     print(name)
+
+# name = "alex"
+# print(name)
+# name = "allistair"
+# print(name)
+# name = "andrew"
+# print(name)
+
+# rng = list(range(6))
+# print(rng)
+
+# lst = [1, 2, 3, 4, 5, 6, 7 , 8]
+# for num in lst:
+#     print(num*2)
+# print(lst)
+
+# #range(6) --> [0, 1, 2, 3, 4, 5]
+# #range(8) --> [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# # range(len(lstr)) --> [0, 1, 2, 3, 4, 5, ... len(lst) - 1]
+
+#  list_length = len(lst)
+# for index in range(list_length):
+#     lst[index] = 2 * lst[index]
+#     print(lst[index])
+
+# print(lst)
+
